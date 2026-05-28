0eaeafaf2f445698b114bef2abb49e534823d03f
Hithesh <hitheshsjecmca@gmail.com>
2026-05-28 09:52:14 +0530

    image upload phase

--- app.py
+++ app.py
@@ -1,47 +1,34 @@
 from flask import Flask, render_template, request
-import joblib
-import numpy as np
+import os
 
 app = Flask(__name__)
 
-# Load trained model
-model = joblib.load("model/fake_profile_model.pkl")
+# Upload folder
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/upload', methods=['POST'])
+def upload():
 
-    features = [
-        float(request.form['profile_pic']),
-        float(request.form['nums_username']),
-        float(request.form['fullname_words']),
-        float(request.form['nums_fullname']),
-        float(request.form['name_username']),
-        float(request.form['description_length']),
-        float(request.form['external_url']),
-        float(request.form['private']),
-        float(request.form['posts']),
-        float(request.form['followers']),
-        float(request.form['follows'])
-    ]
+    if 'profile_image' not in request.files:
+        return "No File Uploaded"
 
-    final_features = np.array(features).reshape(1, -1)
+    file = request.files['profile_image']
 
-    prediction = model.predict(final_features)[0]
+    if file.filename == '':
+        return "No Selected File"
 
-    probability = model.predict_proba(final_features)[0]
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
 
-    confidence = round(max(probability) * 100, 2)
-
-    result = "Fake Profile" if prediction == 1 else "Real Profile"
+    file.save(filepath)
 
     return render_template(
         'result.html',
-        prediction=result,
-        confidence=confidence
+        image_path=filepath
     )
 
 if __name__ == '__main__':
